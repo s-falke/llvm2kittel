a31ef77b579a04b685ecad14cc74079c0346f82b
Stephan Falke <falke@iti.uka.de>
2014-04-30 11:43:22 +0200

    Be more CMake-y with FindGMP.

--- cmake/modules/FindGMP.cmake
+++ cmake/modules/FindGMP.cmake
@@ -1,25 +1,41 @@
-find_path(GMP_INCLUDE_DIR gmpxx.h
-          DOC "GMP include path"
-         )
+# Find GMP (GNU multi precision math library)
+#
+#  GMP_FOUND       - True if GMP was found
+#  GMP_INCLUDE_DIR - Path to GMP's include directory.
+#  GMP_LIBRARY     - Path to libgmp.
+#  GMPXX_LIBRARY   - Path to libgmpxx.
+#  GMP_LIBRARIES   - Path to GMP's libraries.
 
-find_library(GMP_LIBRARY NAMES gmp
-             DOC "GMP libary"
-            )
-find_library(GMPXX_LIBRARY NAMES gmpxx
-             DOC "GMPXX library"
-            )
+include(FindPackageHandleStandardArgs)
 
 if(GMP_INCLUDE_DIR AND GMP_LIBRARY AND GMPXX_LIBRARY)
-  get_filename_component(GMP_LIBRARY_DIR ${GMP_LIBRARY} PATH)
-  get_filename_component(GMPXX_LIBRARY_DIR ${GMPXX_LIBRARY} PATH)
-  set(GMP_CXXFLAGS "-I${GMP_INCLUDE_DIR}")
-  set(GMP_LDFLAGS "-L${GMP_LIBRARY_DIR} -L${GMPXX_LIBRARY_DIR}")
-  set(GMP_LIBS "-lgmp -lgmpxx")
-  set(GMP_FOUND TRUE)
-endif()
 
-if(GMP_FOUND)
-  message(STATUS "Found GMP")
-elseif(NOT GMP_FOUND)
-  message(FATAL_ERROR "Could not find GMP")
-endif()
+  # Already found, don't search again
+  set(GMP_FOUND TRUE)
+
+else(GMP_INCLUDE_DIR AND GMP_LIBRARY AND GMPXX_LIBRARY)
+
+  find_path(GMP_INCLUDE_DIR gmpxx.h DOC "Path to GMP's include directory")
+
+  find_library(GMP_LIBRARY NAMES gmp DOC "Path to GMP's library")
+
+  find_library(GMPXX_LIBRARY NAMES gmpxx DOC "Path to GMP's C++ library")
+
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG
+    GMP_INCLUDE_DIR
+    GMP_LIBRARY
+    GMPXX_LIBRARY
+  )
+
+  set(GMP_LIBRARIES ${GMP_LIBRARY} ${GMPXX_LIBRARY})
+
+  set(GMP_LIBRARIES ${GMP_LIBRARIES} CACHE STRING "Paths to GMP's libraries")
+
+  mark_as_advanced(
+    GMP_INCLUDE_DIR
+    GMP_LIBRARY
+    GMPXX_LIBRARY
+    GMP_LIBRARIES
+  )
+
+endif(GMP_INCLUDE_DIR AND GMP_LIBRARY AND GMPXX_LIBRARY)
