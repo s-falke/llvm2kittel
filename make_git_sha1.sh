ef72b548852f69fbe8d4d3a22fce708fee11d7b0
Stephan Falke <falke@iti.uka.de>
2014-05-05 13:07:19 +0200

    Re-did git SHA1 computation.
    The old approach would usually give me "refs/heads/master", which is
    of limited use.

---
+++ make_git_sha1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OUTFILE=$1
+GIT_SHA1=`git describe --always --dirty --abbrev=40`
+
+cat > ${OUTFILE}.tmp << EOP
+#define THE_GIT_SHA1 "${GIT_SHA1}"
+
+static const char GIT_SHA1[] = THE_GIT_SHA1;
+
+const char *get_git_sha1()
+{
+    return GIT_SHA1;
+}
+EOP
+
+if [ -e ${OUTFILE} ]
+then
+  diff ${OUTFILE} ${OUTFILE}.tmp --brief > /dev/null
+  CHANGED=$?
+  if [ ${CHANGED} -eq 0 ]
+  then
+    # unchanged
+    rm -f ${OUTFILE}.tmp
+    exit
+  fi
+fi
+
+mv -f ${OUTFILE}.tmp ${OUTFILE}
+rm -f ${OUTFILE}.tmp
