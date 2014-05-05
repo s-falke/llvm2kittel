795331d0d85d9885c4cea38f31b041cb0107da2d
Stephan Falke <falke@iti.uka.de>
2014-05-05 12:00:00 +0200

    Having "." at the end looks odd.

--- tools/llvm2kittel.cpp
+++ tools/llvm2kittel.cpp
@@ -80,9 +80,9 @@
 void versionPrinter()
 {
     std::cout << "llvm2KITTeL" << std::endl;
-    std::cout << "Copyright 2010-2014 by Stephan Falke." << std::endl;
+    std::cout << "Copyright 2010-2014 Stephan Falke" << std::endl;
     std::cout << "Version " << get_git_sha1();
-    std::cout << ", using LLVM " << LLVM_MAJOR << "." << LLVM_MINOR << "." << std::endl;
+    std::cout << ", using LLVM " << LLVM_MAJOR << "." << LLVM_MINOR << std::endl;
 }
 
 static cl::opt<std::string> filename(cl::Positional, cl::Required, cl::desc("<input bitcode>"), cl::init(std::string()));
